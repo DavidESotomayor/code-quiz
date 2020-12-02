8264a267d569c816c80f8259112c9a2ea92c4d06
David Sotomayor <david.elijah.sotomayor@gmail.com>
2020-12-02 14:55:16 -0600

    added header

---
+++ script.js
@@ -0,0 +1,9 @@
+// Create Layout
+// Create Start button
+    // Clicking Start button starts timer
+    // A question is then prompted
+    // A wrong answer subtracts time from timer
+    // A right answer is logged
+// When timer reaches zero
+    // Game is over; submit initials and score
+    // Initials and score are saved; option to play quiz again
