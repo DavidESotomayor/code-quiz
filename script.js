c1d1fa6a1cf604cfa4c88d8428adaf15f4af268f
David Sotomayor <david.elijah.sotomayor@gmail.com>
2020-12-03 11:54:19 -0600

    added display box button functionality

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-// getting all elements
+// Selecting Elements
 var startButton = document.querySelector(".startButton button");
 var displayBox = document.querySelector(".displayBox");
 var quitButton = displayBox.querySelector(".displayBoxButtons .quit");
 var continueButton = displayBox.querySelector(".displayBoxButtons .continue")
+var quizBox = document.querySelector(".quizBox");
 
-
+// Start Quiz Button Click Functionality
+//displays "Display Box"
 startButton.onclick = function () {
     displayBox.classList.add("activeDisplayBox");
 }
+
+// Exit Quiz Button Click Functionality
+// hides "Display Box"
+quitButton.onclick = function () {
+    displayBox.classList.remove("activeDisplayBox");
+}
+
+
+// Continue Button Click Functionality
+//displays "Quiz Box"
+continueButton.onclick = function () {
+    displayBox.classList.remove("activeDisplayBox");
+    quizBox.classList.add("activeQuizBox");
+}
+
+
     // Clicking Start button starts timer
     // A question is then prompted
     // A wrong answer subtracts time from timer
